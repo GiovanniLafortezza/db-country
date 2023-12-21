66f870153e8b01c5c9cf0decd542a9992d25c1e0
GiovanniLafortezza <recordeknot388@gmail.com>
2023-12-21 10:33:43 +0100

    add : step 3

--- db_country.sql
+++ db_country.sql
@@ -6,3 +6,10 @@ WHERE c.name LIKE 'P%' AND c.area > 1000;
 -- 2. Contare quante nazioni sono presenti nel database
 SELECT COUNT(*) AS total_countries
 FROM countries;
+
+-- 3. Selezionare il nome delle regioni del continente europeo, in ordine alfabetico
+SELECT *
+FROM regions r  
+JOIN continents c ON c.continent_id = r.continent_id 
+WHERE c.name = 'Europe'
+ORDER BY r.name ASC;
